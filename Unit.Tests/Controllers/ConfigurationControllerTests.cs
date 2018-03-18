c2c13d22989a59bd61a897f92feeaf9e46f4a45a
Mikhael Regni <mikhael@uping.be>
2018-03-18 10:58:26 +0100

    Added tests

---
+++ Unit.Tests/Controllers/ConfigurationControllerTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using EmbyStat.Controllers.Configuration;
+using EmbyStat.Controllers.Helpers;
+using EmbyStat.Repositories.Config;
+using EmbyStat.Services.Config;
+using FluentAssertions;
+using MediaBrowser.Common.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+using static Xunit.Assert;
+
+namespace Unit.Tests.Controllers
+{
+	public class ConfigurationControllerTests
+	{
+		private readonly ConfigurationController _subject;
+		private readonly Configuration _configuration;
+		private readonly Mock<IConfigurationService> _configurationServiceMock;
+		private readonly Mock<ILogger<ConfigurationController>> _LoggerMock;
+
+		public ConfigurationControllerTests()
+		{
+			 _configuration = new Configuration()
+			{
+				AccessToken = "1234567890",
+				EmbyServerAddress = "https://localhost:8096",
+				EmbyUserName = "admin",
+				Id = "0987654321",
+				Language = "en",
+				UserId = "12345",
+				Username = "usernameAdmin",
+				WizardFinished = false
+			};
+
+			_configurationServiceMock = new Mock<IConfigurationService>();
+			_configurationServiceMock.Setup(x => x.GetServerSettings()).Returns(_configuration);
+
+			_LoggerMock = new Mock<ILogger<ConfigurationController>>();
+
+			_subject = new ConfigurationController(_configurationServiceMock.Object, _LoggerMock.Object);
+			Mapper.Initialize(cfg => cfg.AddProfile<MapProfiles>());
+		}
+
+		[Fact]
+		public void TestIfConfigurationIsLoadedCorrect()
+		{
+			var result = _subject.Get();
+
+			result.Should().BeOfType<OkObjectResult>();
+			_configurationServiceMock.Verify(x => x.GetServerSettings(), Times.Once);
+		}
+	}
+}
