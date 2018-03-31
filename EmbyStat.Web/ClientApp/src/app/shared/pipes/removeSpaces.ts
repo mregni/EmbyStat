d699d8655138aef242f59cb2a9efb1ae1eb53b63
Mikhaël Regni <mikhael@uping.be>
2018-03-31 16:30:49 +0200

    remove unused pipes

--- EmbyStat.Web/ClientApp/src/app/shared/pipes/removeSpaces.ts
+++
@@ -1,8 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'removeSpaces' })
-export class RemoveSpaces implements PipeTransform {
-  transform(value) {
-    return value.replace(/ /g, "");
-  }
-}
