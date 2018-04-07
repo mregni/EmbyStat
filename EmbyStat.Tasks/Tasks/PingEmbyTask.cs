122fee0c31c278251a42768b286cec337464d360
Mikhaël Regni <mikhael@uping.be>
2018-04-08 01:01:15 +0200

    Theme fix

--- EmbyStat.Tasks/Tasks/PingEmbyTask.cs
+++ EmbyStat.Tasks/Tasks/PingEmbyTask.cs
@@ -27,13 +27,13 @@ namespace EmbyStat.Tasks.Tasks
             return Task.Run(() =>
             {
                 progress.Report(10);
-                Thread.Sleep(10000);
+                Thread.Sleep(1000);
                 progress.Report(20);
-                Thread.Sleep(10000);
+                Thread.Sleep(1000);
                 progress.Report(40);
-                Thread.Sleep(10000);
+                Thread.Sleep(1000);
                 progress.Report(60);
-                Thread.Sleep(10000);
+                Thread.Sleep(1000);
                 progress.Report(75);
                 Thread.Sleep(1000);
                 progress.Report(80);
