9ca71dc6623a931a81e65e4f6420c59d9c5bfd8e
Mikhaël Regni <mikhael@uping.be>
2018-04-28 20:55:17 +0200

    Added test graph, #43

--- EmbyStat.Web/ClientApp/src/app/movie/movie-charts/movie-charts.component.ts
+++ EmbyStat.Web/ClientApp/src/app/movie/movie-charts/movie-charts.component.ts
@@ -6,7 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-charts.component.scss']
 })
 export class MovieChartsComponent implements OnInit {
-
+  public view: number[] = [700, 400];
+  public doughnut: boolean = false;
+  public data: any = [
+    {
+      "name": "Germany",
+      "value": 8940000
+    },
+    {
+      "name": "USA",
+      "value": 5000000
+    },
+    {
+      "name": "France",
+      "value": 7200000
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
