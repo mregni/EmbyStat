eb18152045b7220e14e8e91378d6ca0f08a8d511
Mikhaël Regni <mikhael@uping.be>
2018-05-06 19:33:47 +0200

    basic swimlane implementation

--- EmbyStat.Web/ClientApp/src/app/movie/movie-charts/movie-charts.component.ts
+++ EmbyStat.Web/ClientApp/src/app/movie/movie-charts/movie-charts.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit  } from '@angular/core';
 
 @Component({
   selector: 'app-movie-charts',
   templateUrl: './movie-charts.component.html',
   styleUrls: ['./movie-charts.component.scss']
 })
-export class MovieChartsComponent implements OnInit {
+export class MovieChartsComponent implements OnInit, AfterViewInit  {
   private _selectedCollections: string[];
 
   get selectedCollections(): string[] {
@@ -22,41 +22,46 @@ export class MovieChartsComponent implements OnInit {
     this._selectedCollections = collection;
   }
 
-  public view: number[] = [700, 350];
+  public view: number[] = [0, 0];
   public doughnut: boolean = false;
-  public data: any = [
-    {
-      "name": "Germany",
-      "value": 40632
-    },
-    {
-      "name": "France",
-      "value": 36745
-    },
-    {
-      "name": "United Kingdom",
-      "value": 36240
-    },
-    {
-      "name": "Spain",
-      "value": 33000
-    },
-    {
-      "name": "Uganda",
-      "value": 19294
-    },
-    {
-      "name": "Bonaire, Sint Eustatius and Saba",
-      "value": 50184
-    },
-    {
-      "name": "India",
-      "value": 20303
-    }
-  ];
+  public data: any;
   constructor() { }
 
   ngOnInit() {
+    
+  }
+
+  ngAfterViewInit(): void {
+    this.data = [
+      {
+        "name": "Germany",
+        "value": 40632
+      },
+      {
+        "name": "France",
+        "value": 36745
+      },
+      {
+        "name": "United Kingdom",
+        "value": 36240
+      },
+      {
+        "name": "Spain",
+        "value": 33000
+      },
+      {
+        "name": "Uganda",
+        "value": 19294
+      },
+      {
+        "name": "Bonaire, Sint Eustatius and Saba",
+        "value": 50184
+      },
+      {
+        "name": "India",
+        "value": 20303
+      }
+    ];
   }
 
 }
