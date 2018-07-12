aa648a34f1f9dc447d7fd08306ee073f80d30f30
Mikhaël Regni <mikhael@uping.be>
2018-07-12 21:47:07 +0200

    Added language service to frontend #102

---
+++ EmbyStat.Web/ClientApp/src/app/shared/components/language/models/language.ts
@@ -0,0 +1,4 @@
+export class Language {
+  public name: string;
+  public code: string;
+}
---
+++ EmbyStat.Web/ClientApp/src/app/shared/components/language/service/language.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+import { Language } from '../models/language';
+
+@Injectable()
+export class LanguageService {
+  private readonly getLangaugesUrl: string = '/language/getlist';
+
+  constructor(private http: HttpClient) { }
+
+  getLanguages(): Observable<Language[]> {
+    return this.http.get<Language[]>('/api' + this.getLangaugesUrl);
+  }
+}
