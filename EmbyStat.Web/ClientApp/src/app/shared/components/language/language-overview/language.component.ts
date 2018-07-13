04f771736e6d72b80eab6f62b6b998c804ec49fc
Mikhaël Regni <mikhael@uping.be>
2018-07-13 10:03:34 +0200

    Moving files around #102

---
+++ EmbyStat.Web/ClientApp/src/app/shared/components/language/language-overview/language.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Language } from '../models/language';
+@Component({
+  selector: 'app-language',
+  templateUrl: './language.component.html',
+  styleUrls: ['./language.component.scss']
+})
+
+export class LanguageComponent implements OnInit {
+  @Input() language: Language;
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+}
