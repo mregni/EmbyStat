22bf5f2bd61b008bf398bc0130bd14b4dae10a62
Mikhaël Regni <mikhael@uping.be>
2018-07-13 10:43:02 +0200

    Added all languages to database #102

--- EmbyStat.Repositories/DatabaseInitializer.cs
+++ EmbyStat.Repositories/DatabaseInitializer.cs
@@ -64,7 +64,20 @@ namespace EmbyStat.Repositories
 	            new Language { Id = Guid.NewGuid().ToString(), Name = "Nederlands", Code = "nl-NL" },
 	            new Language { Id = Guid.NewGuid().ToString(), Name = "English", Code = "en-US" },
 	            new Language { Id = Guid.NewGuid().ToString(), Name = "Deutsche", Code = "de-DE" },
-	        };
+	            new Language { Id = Guid.NewGuid().ToString(), Name = "Dansk", Code = "da-DK" },
+	            new Language { Id = Guid.NewGuid().ToString(), Name = "Ελληνικά", Code = "el-GR" },
+	            new Language { Id = Guid.NewGuid().ToString(), Name = "Español", Code = "es-ES" },
+	            new Language { Id = Guid.NewGuid().ToString(), Name = "Suomi", Code = "fi-FI" },
+	            new Language { Id = Guid.NewGuid().ToString(), Name = "Français", Code = "fr-FR" },
+	            new Language { Id = Guid.NewGuid().ToString(), Name = "Magyar", Code = "hu-HU" },
+	            new Language { Id = Guid.NewGuid().ToString(), Name = "Italiano", Code = "it-IT" },
+	            new Language { Id = Guid.NewGuid().ToString(), Name = "Norsk", Code = "no-NO" },
+	            new Language { Id = Guid.NewGuid().ToString(), Name = "Polski", Code = "pl-PL" },
+	            new Language { Id = Guid.NewGuid().ToString(), Name = "Brasileiro", Code = "pt-BR" },
+	            new Language { Id = Guid.NewGuid().ToString(), Name = "Português", Code = "pt-PT" },
+	            new Language { Id = Guid.NewGuid().ToString(), Name = "Românesc", Code = "ro-RO" },
+	            new Language { Id = Guid.NewGuid().ToString(), Name = "Svenska", Code = "sv-SE" }
+            };
 
 	        _context.Languages.AddRange(languages);
 	        await _context.SaveChangesAsync();
