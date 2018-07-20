7a2683fdf9cc67f50713ccf984d79de302e6b157
Mikhaël Regni <mikhael@uping.be>
2018-07-20 10:29:23 +0200

    Added task status to toolbar #154

--- EmbyStat.Web/ClientApp/src/app/shared/toolbar/toolbar.component.ts
+++ EmbyStat.Web/ClientApp/src/app/shared/toolbar/toolbar.component.ts
@@ -1,24 +1,45 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { HubConnection } from '@aspnet/signalr';
 
 import { ConfigurationFacade } from '../../configuration/state/facade.configuration';
 import { Configuration } from '../../configuration/models/configuration';
+import { Task } from '../../task/models/task';
+
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
-
+  private hubConnection: HubConnection;
   public configuration$: Observable<Configuration>;
+  public runningTask: Task;
 
   @Output() toggleSideNav = new EventEmitter<void>();
 
   constructor(private configurationFacade: ConfigurationFacade) {
+    this.hubConnection = new HubConnection('/tasksignal');
     this.configuration$ = configurationFacade.configuration$;
+
+    this.hubConnection.on('ReceiveInfo', (data: Task[]) => {
+      const task = data.find(x => x.state === 2);
+      if (data.some(x => x.state === 2)) {
+        this.runningTask = task;
+      } else {
+        this.runningTask = undefined;
+      }
+    });
+
+    this.hubConnection.on('ReceiveLog', (data: string) => {});
   }
 
   ngOnInit() {
-
+    this.hubConnection
+      .start()
+      .then(() => {
+        console.log('Connection started!');
+      })
+      .catch(err => console.log('Error while establishing connection :('));
   }
 }
