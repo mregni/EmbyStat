03a55d8c55fe23bc1bd281d3e11a555632de0ade
Mikhaël Regni <mikhael@uping.be>
2018-07-23 17:46:03 +0200

    Added about service to backend #169

---
+++ EmbyStat.Services/AboutService.cs
@@ -0,0 +1,28 @@
+﻿using EmbyStat.Services.Interfaces;
+using EmbyStat.Services.Models.About;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Microsoft.DotNet.PlatformAbstractions;
+
+namespace EmbyStat.Services
+{
+    public class AboutService : IAboutService
+    {
+        public About GetAbout()
+        {
+            var version = Assembly.GetEntryAssembly().GetName().Version;
+            var operatingSystem = RuntimeEnvironment.OperatingSystem;
+            var operatingSystemVersion = RuntimeEnvironment.OperatingSystemVersion;
+            var architecture = RuntimeEnvironment.RuntimeArchitecture;
+
+            return new About { 
+                Version = version.ToString(),
+                OperatingSystem = operatingSystem,
+                OperatingSystemVersion = operatingSystemVersion,
+                Architecture = architecture
+            };
+        }
+    }
+}
