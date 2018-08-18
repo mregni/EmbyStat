b3f6be0e4ce7c6ba9956b613ec09ac86c2490d2f
Mikhaël Regni <mikhael@uping.be>
2018-08-18 09:00:37 +0200

    Added new Updater console app project #8

---
+++ Updater/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Exceptions.Core;
+
+namespace Updater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SetupLogger();
+            Log.Information("Starting update process");
+
+            Log.Information("Update process finished, restarting EmbyStat");
+        }
+
+        private static void SetupLogger()
+        {
+            if (!Directory.Exists("Logs"))
+            {
+                Directory.CreateDirectory("Logs");
+            }
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.WithExceptionDetails(new DestructuringOptionsBuilder().WithDefaultDestructurers().WithRootName("Exception"))
+                .Enrich.FromLogContext()
+                .WriteTo.File("Logs/updater.txt", outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .CreateLogger();
+        }
+    }
+}
