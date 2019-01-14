8abed999c831e3115c95a9d61c396e65fc9bc598
Mikhaël Regni <mikhael@uping.be>
2019-01-14 17:12:37 +0100

    Removed unused code

--- EmbyStat.Web/ClientApp/src/app/shared/dialogs/sync-is-running/sync-is-running.component.ts
+++ EmbyStat.Web/ClientApp/src/app/shared/dialogs/sync-is-running/sync-is-running.component.ts
@@ -1,25 +1,21 @@
 /* tslint:disable:component-class-suffix */
-import { Component, Inject, OnDestroy } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { Subscription } from 'rxjs/Subscription';
 import { Router } from '@angular/router';
-
-import { JobSocketService } from '../../services/job-socket.service';
 
 @Component({
   selector: 'app-sync-is-running',
   templateUrl: './sync-is-running.component.html',
   styleUrls: ['./sync-is-running.component.scss']
 })
-export class SyncIsRunningDialog implements OnDestroy {
+export class SyncIsRunningDialog implements {
   private getTaskInfuSub: Subscription;
 
   constructor(
     public dialogRef: MatDialogRef<string>,
     @Inject(MAT_DIALOG_DATA) public data: string,
-    private jobSocketService: JobSocketService,
     private router: Router) {
-    this.getTaskInfuSub = this.jobSocketService.infoSubject.subscribe();
   }
 
   cancelClick(): void {
@@ -29,11 +25,5 @@ export class SyncIsRunningDialog implements OnDestroy {
   goToJobs(): void {
     this.router.navigate(['/job']);
     this.dialogRef.close();
-  }
-
-  ngOnDestroy() {
-    if (this.getTaskInfuSub !== undefined) {
-      this.getTaskInfuSub.unsubscribe();
-    }
   }
 }
