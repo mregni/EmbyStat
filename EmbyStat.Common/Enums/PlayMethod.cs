419f9ec37ce92454af1de2be47ef7b8bf39a7dd2
Mikhaël Regni <mikhael@uping.be>
2019-02-06 11:40:19 +0100

    Added enums that are needed for the Sessions table

---
+++ EmbyStat.Common/Enums/PlayMethod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmbyStat.Common.Enums
+{
+    public enum PlayMethod
+    {
+        Transcode = 0,
+        DirectStream = 1,
+        DirectPlay = 2,
+    }
+}
---
+++ EmbyStat.Common/Enums/RepeatMode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmbyStat.Common.Enums
+{
+    public enum RepeatMode
+    {
+        RepeatNone = 0,
+        RepeatAll = 1,
+        RepeatOne = 2
+    }
+}
