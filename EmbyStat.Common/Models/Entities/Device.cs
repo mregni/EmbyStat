6cda7094fcfcabbcfd9f6de39abc71c23bde3251
Mikhaël Regni <mikhael@uping.be>
2019-02-06 11:54:48 +0100

    Update database model

--- EmbyStat.Common/Models/Entities/Device.cs
+++ EmbyStat.Common/Models/Entities/Device.cs
@@ -1,11 +1,17 @@
-﻿namespace EmbyStat.Common.Models.Entities
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmbyStat.Common.Models.Entities
 {
     public class Device
     {
+        [Key]
 		public string Id { get; set; }
+        public string Name { get; set; }
 		public string AppName { get; set; }
 		public string AppVersion { get; set; }
-		public string Name { get; set; }
-		public string LastUserName { get; set; }
+        public string LastUserId { get; set; }
+        public DateTime DateLastActivity { get; set; }
+        public string IconUrl { get; set; }
 	}
 }
