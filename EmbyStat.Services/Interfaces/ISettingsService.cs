1c492cf9440db4a46d1142b98245afa7ff2fe4ba
Mikhaël Regni <mikhael@uping.be>
2019-02-10 01:32:22 +0100

    Implemented settings server, will use seperate json file for user settings #318 #326 #256

---
+++ EmbyStat.Services/Interfaces/ISettingsService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using EmbyStat.Common.Models;
+using EmbyStat.Common.Models.Settings;
+
+namespace EmbyStat.Services.Interfaces
+{
+    public interface ISettingsService
+    {
+        event EventHandler<GenericEventArgs<UserSettings>> OnUserSettingsChanged;
+        AppSettings GetAppSettings();
+        UserSettings GetUserSettings();
+        Task SaveUserSettingsToFile(UserSettings userSettings);
+        Task SetUpdateInProgressSetting(bool value);
+    }
+}
---
+++ EmbyStat.Services/SettingsService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using EmbyStat.Common.Models;
+using EmbyStat.Common.Models.Settings;
+using EmbyStat.Services.Interfaces;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace EmbyStat.Services
+{
+    public class SettingsService : ISettingsService
+    {
+        private readonly AppSettings _appSettings;
+        private UserSettings _userSettings;
+        public event EventHandler<GenericEventArgs<UserSettings>> OnUserSettingsChanged;
+
+        public SettingsService(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+            LoadUserSettingsFromFile();
+        }
+
+        public AppSettings GetAppSettings()
+        {
+            return _appSettings;
+        }
+
+        public UserSettings GetUserSettings()
+        {
+            return _userSettings;
+        }
+
+        public async Task SaveUserSettingsToFile(UserSettings userSettings)
+        {
+            _userSettings = userSettings;
+            var strJson = JsonConvert.SerializeObject(userSettings, Formatting.Indented);
+            var dir = Path.Combine(_appSettings.Dirs.Settings, "usersettings.json");
+            await File.WriteAllTextAsync(dir, strJson);
+            OnUserSettingsChanged?.Invoke(this, new GenericEventArgs<UserSettings>(_userSettings));
+        }
+
+        public async Task SetUpdateInProgressSetting(bool value)
+        {
+            _userSettings.UpdateInProgress = value;
+            await SaveUserSettingsToFile(_userSettings);
+        }
+
+        private void LoadUserSettingsFromFile()
+        {
+            var dir = Path.Combine(_appSettings.Dirs.Settings, "usersettings.json");
+            _userSettings = JsonConvert.DeserializeObject<UserSettings>(File.ReadAllText(dir));
+            OnUserSettingsChanged?.Invoke(this, new GenericEventArgs<UserSettings>(_userSettings));
+        }
+    }
+}
