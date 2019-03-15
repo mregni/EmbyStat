41d563ebffa46d853d780c7b1fe40b23665fe318
Mikhaël Regni <mikhael@uping.be>
2019-03-15 07:57:22 +0100

    Moved GitHub client
    Signed-off-by: Mikhaël Regni <mikhael@uping.be>

--- EmbyStat.Clients.GitHub/Models/ReleaseObject.cs
+++ EmbyStat.Clients.GitHub/Models/ReleaseObject.cs
@@ -1,25 +1,43 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace EmbyStat.Clients.Github.Models
 {
     public class ReleaseObject
     {
-        public string tarball_url { get; set; }
-        public List<Asset> assets { get; set; }
-        public string published_at { get; set; }
-        public string created_at { get; set; }
-        public bool prerelease { get; set; }
-        public Author author { get; set; }
-        public bool draft { get; set; }
-        public string name { get; set; }
-        public string target_commitish { get; set; }
-        public string tag_name { get; set; }
-        public int id { get; set; }
-        public string html_url { get; set; }
-        public string upload_url { get; set; }
-        public string assets_url { get; set; }
-        public string url { get; set; }
-        public string zipball_url { get; set; }
-        public string body { get; set; }
+        [JsonProperty("tarball_url")]
+        public string TarballUrl { get; set; }
+        [JsonProperty("assets")]
+        public List<Asset> Assets { get; set; }
+        [JsonProperty("published_at")]
+        public string PublishedAt { get; set; }
+        [JsonProperty("created_at")]
+        public string CreatedAt { get; set; }
+        [JsonProperty("prerelease")]
+        public bool PreRelease { get; set; }
+        [JsonProperty("author")]
+        public Author Author { get; set; }
+        [JsonProperty("draft")]
+        public bool Draft { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("target_commitish")]
+        public string TargetCommitish { get; set; }
+        [JsonProperty("tag_name")]
+        public string TagName { get; set; }
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("html_url")]
+        public string HtmlUrl { get; set; }
+        [JsonProperty("upload_url")]
+        public string UploadUrl { get; set; }
+        [JsonProperty("assets_url")]
+        public string AssetsUrl { get; set; }
+        [JsonProperty("url")]
+        public string Url { get; set; }
+        [JsonProperty("zipball_url")]
+        public string ZipballUrl { get; set; }
+        [JsonProperty("body")]
+        public string Body { get; set; }
     }
 }
