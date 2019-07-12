785df8cc0791ec04294ed4bfb95c009eacfe086b
Mikhaël Regni <mikhael@uping.be>
2019-07-12 09:36:45 +0200

    Changed default port to 6543 #725

--- EmbyStat.Web/StartupOptions.cs
+++ EmbyStat.Web/StartupOptions.cs
@@ -4,7 +4,7 @@ namespace EmbyStat.Web
 {
     public class StartupOptions
     {
-        [Option("port", Required = false, Default = 5432, HelpText = "Set the port EmbyStat needs to be hosted on")]
+        [Option("port", Required = false, Default = 6543, HelpText = "Set the port EmbyStat needs to be hosted on")]
         public int Port { get; set; }
     }
 }
