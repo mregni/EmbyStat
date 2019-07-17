9024b3f81d2dc0d742d0b25af3d8542a13f4099a
Mikhaël Regni <mikhael@uping.be>
2019-07-17 10:00:48 +0200

    Using new version variable

--- Scripts/appsettings.sh
+++ Scripts/appsettings.sh
@@ -1,4 +1,4 @@
 sed -i 's/win10-x64-v{version}.zip/ubuntu.16.10-x64-v{version}.tar.gz/g' EmbyStat.Web/appsettings.json
-sed -i 's/0.0.0.0/$(BuildVersion.MajorVersion).$($env:BuildVersion.MinorVersion).$($env:BuildVersion.PatchVersion).$($env:BuildVersion.BuildCounter)/g' EmbyStat.Web/appsettings.json
+sed -i 's/0.0.0.0/$(GitVersion.SemVer)/g' EmbyStat.Web/appsettings.json
 sed -i 's/RollbarENV/dev/g' EmbyStat.Web/appsettings.json
 sed -i 's/XXXXXXXXXX/$(RollbarKey)/g' EmbyStat.Web/appsettings.json
