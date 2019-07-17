a64e504b4135c9032e9e5a23da589dbbe413f257
Mikhaël Regni <mikhael@uping.be>
2019-07-17 14:32:46 +0200

    Trying to use a seperate variable

--- Scripts/appsettings.ps1
+++ Scripts/appsettings.ps1
@@ -1,6 +1,7 @@
 $appSettings = "EmbyStat.Web\appsettings.json"
 $version = "$($env:GitVersion_SemVer)"
+$rollbarKey = "$(RollbarKey)"
 
 (GC $appSettings).Replace("0.0.0.0", "$version") | Set-Content $appSettings
 (GC $appSettings).Replace("RollbarENV", "dev") | Set-Content $appSettings
-(GC $appSettings).Replace("XXXXXXXXXX", "$($env:RollbarKey)") | Set-Content $appSettings
+(GC $appSettings).Replace("XXXXXXXXXX", "$rollbarKey") | Set-Content $appSettings
