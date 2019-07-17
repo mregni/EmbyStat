d9aac03ed4573fdfe2c0666e80419ebd298076f1
Mikhaël Regni <mikhael@uping.be>
2019-07-17 20:15:16 +0200

    Trying again

--- Scripts/appsettings.ps1
+++ Scripts/appsettings.ps1
@@ -2,6 +2,7 @@ param($updatePackage)
 
 $appSettings = "EmbyStat.Web\appsettings.json"
 $version = "$($env:GitVersion_SemVer)"
+$rollbar = "$(rollbarkey)"
 
 Write-Host "Updating version to $version";
 (GC $appSettings).Replace("0.0.0.0", "$version") | Set-Content $appSettings
@@ -10,7 +11,7 @@ Write-Host "Updating RollbarENV to dev";
 (GC $appSettings).Replace("RollbarENV", "dev") | Set-Content $appSettings
 
 Write-Host "Updating Rollbar key to $($env:rollbarkey)";
-(GC $appSettings).Replace("XXXXXXXXXX", "$($env:rollbarkey)") | Set-Content $appSettings
+(GC $appSettings).Replace("XXXXXXXXXX", "$rollbar") | Set-Content $appSettings
 
 Write-Host "Updating update package name to $updatePackage"
 (GC $appSettings).Replace("win10-x86", "$updatePackage") | Set-Content $appSettings
