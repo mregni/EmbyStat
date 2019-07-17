c96dc06ecdb5765f02041340e22328afd3e38680
Mikhaël Regni <mikhael@uping.be>
2019-07-17 14:54:22 +0200

    Using azure devops secret

--- Scripts/appsettings.ps1
+++ Scripts/appsettings.ps1
@@ -1,7 +1,7 @@
 $appSettings = "EmbyStat.Web\appsettings.json"
 $version = "$($env:GitVersion_SemVer)"
-$rollbarKey = "$($env:RollbarKey)"
+$rollbarKey = "$($env:webapp.rollbar.key)"
 
 (GC $appSettings).Replace("0.0.0.0", "$version") | Set-Content $appSettings
 (GC $appSettings).Replace("RollbarENV", "dev") | Set-Content $appSettings
-(GC $appSettings).Replace("XXXXXXXXXX", $rollbarKey) | Set-Content $appSettings
+(GC $appSettings).Replace("XXXXXXXXXX", "$rollbarKey") | Set-Content $appSettings
