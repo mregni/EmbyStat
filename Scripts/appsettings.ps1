52bfac7a9899bbc0b77ea086071196d6d31d1a84
Mikhaël Regni <mikhael@uping.be>
2019-07-17 16:11:15 +0200

    Updating git status

--- Scripts/appsettings.ps1
+++ Scripts/appsettings.ps1
@@ -1,6 +1,5 @@
 $appSettings = "EmbyStat.Web\appsettings.json"
 $version = "$($env:GitVersion_SemVer)"
-$rollbarKey = "$($env:webapp_rollbar_key)"
 
 Write-Host "Updating version to $version";
 (GC $appSettings).Replace("0.0.0.0", "$version") | Set-Content $appSettings
@@ -9,5 +8,5 @@ Write-Host "Updating version to $version";
 Write-Host "Updating RollbarENV to dev";
 (GC $appSettings).Replace("RollbarENV", "dev") | Set-Content $appSettings
 
-Write-Host "Updating Rollbar key to $rollbarKey";
-(GC $appSettings).Replace("XXXXXXXXXX", "$rollbarKey") | Set-Content $appSettings
+Write-Host "Updating Rollbar key to $($env:webapp-rollbar-key)";
+(GC $appSettings).Replace("XXXXXXXXXX", "$($env:webapp-rollbar-key)") | Set-Content $appSettings
