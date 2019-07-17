63f785c6ef894032e9243b9b016fa39f4c82460b
Mikhaël Regni <mikhael@uping.be>
2019-07-17 07:43:46 +0200

    Updating script to use env variables

--- Scripts/appsettings.ps1
+++ Scripts/appsettings.ps1
@@ -1,6 +1,6 @@
 $appSettings = "EmbyStat.Web\appsettings.json"
-$version = "$(BuildVersion.MajorVersion).$(BuildVersion.MinorVersion).$(BuildVersion.PatchVersion).$(BuildVersion.BuildCounter)"
+$version = "$($env:BuildVersion.MajorVersion).$($env:BuildVersion.MinorVersion).$($env:BuildVersion.PatchVersion).$($env:BuildVersion.BuildCounter)"
 
 (GC $appSettings).Replace("0.0.0.0", "$version") | Set-Content $appSettings
 (GC $appSettings).Replace("RollbarENV", "dev") | Set-Content $appSettings
-(GC $appSettings).Replace("XXXXXXXXXX", "$(RollbarKey)") | Set-Content $appSettings
+(GC $appSettings).Replace("XXXXXXXXXX", "$($env:RollbarKey)") | Set-Content $appSettings
