fda2d2943dd1b3b0f9d38051a7fb7e7d20f86eb0
Mikhaël Regni <mikhael@uping.be>
2019-07-17 14:35:19 +0200

    Testing substring

--- Scripts/appsettings.ps1
+++ Scripts/appsettings.ps1
@@ -4,4 +4,4 @@ $rollbarKey = "$(RollbarKey)"
 
 (GC $appSettings).Replace("0.0.0.0", "$version") | Set-Content $appSettings
 (GC $appSettings).Replace("RollbarENV", "dev") | Set-Content $appSettings
-(GC $appSettings).Replace("XXXXXXXXXX", "$rollbarKey") | Set-Content $appSettings
+(GC $appSettings).Replace("XXXXXXXXXX", $rollbarKey.substring(0,10)) | Set-Content $appSettings
