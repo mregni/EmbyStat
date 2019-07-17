f7cbdbce5b47c312d792c02c73eb1074eccdb028
Mikhaël Regni <mikhael@uping.be>
2019-07-17 14:19:10 +0200

    Trying to access rollbar secret

--- Scripts/appsettings.ps1
+++ Scripts/appsettings.ps1
@@ -3,4 +3,4 @@ $version = "$($env:GitVersion_SemVer)"
 
 (GC $appSettings).Replace("0.0.0.0", "$version") | Set-Content $appSettings
 (GC $appSettings).Replace("RollbarENV", "dev") | Set-Content $appSettings
-(GC $appSettings).Replace("XXXXXXXXXX", "$($env:RollbarKey)") | Set-Content $appSettings
+(GC $appSettings).Replace("XXXXXXXXXX", "$(RollbarKey)") | Set-Content $appSettings
