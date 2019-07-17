6e2eae8fd0690da205e4f46f737e2d3014aa965a
Mikhaël Regni <mikhael@uping.be>
2019-07-17 16:16:05 +0200

    Using new secret name

--- Scripts/appsettings.sh
+++ Scripts/appsettings.sh
@@ -1,4 +1,4 @@
 sed -i 's/win10-x64-v{version}.zip/ubuntu.16.10-x64-v{version}.tar.gz/g' EmbyStat.Web/appsettings.json
 sed -i 's/0.0.0.0/$(GitVersion.SemVer)/g' EmbyStat.Web/appsettings.json
 sed -i 's/RollbarENV/dev/g' EmbyStat.Web/appsettings.json
-sed -i 's/XXXXXXXXXX/$(webapp-rollbar-key)/g' EmbyStat.Web/appsettings.json
+sed -i 's/XXXXXXXXXX/$(rollbarkey)/g' EmbyStat.Web/appsettings.json
