13a706e9e7b8abe8dc88b56a3129d24e96afeb6e
Mikhaël Regni <mikhael@uping.be>
2019-07-17 15:12:23 +0200

    Adding output logging

--- Scripts/appsettings.ps1
+++ Scripts/appsettings.ps1
@@ -2,6 +2,12 @@ $appSettings = "EmbyStat.Web\appsettings.json"
 $version = "$($env:GitVersion_SemVer)"
 $rollbarKey = "$($env:webapp_rollbar_key)"
 
+Write-Host "Updating version to $version";
 (GC $appSettings).Replace("0.0.0.0", "$version") | Set-Content $appSettings
+
+
+Write-Host "Updating RollbarENV to dev";
 (GC $appSettings).Replace("RollbarENV", "dev") | Set-Content $appSettings
+
+Write-Host "Updating Rollbar key to $rollbarKey";
 (GC $appSettings).Replace("XXXXXXXXXX", "$rollbarKey") | Set-Content $appSettings
