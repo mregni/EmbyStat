8268994602c3118f279143bd47315d24f7f214fb
Mikhaël Regni <mikhael@uping.be>
2019-07-17 14:38:57 +0200

    Updating to use $env: again

--- Scripts/appsettings.ps1
+++ Scripts/appsettings.ps1
@@ -1,6 +1,6 @@
 $appSettings = "EmbyStat.Web\appsettings.json"
 $version = "$($env:GitVersion_SemVer)"
-$rollbarKey = "$(RollbarKey)"
+$rollbarKey = "$($env:RollbarKey)"
 
 (GC $appSettings).Replace("0.0.0.0", "$version") | Set-Content $appSettings
 (GC $appSettings).Replace("RollbarENV", "dev") | Set-Content $appSettings
