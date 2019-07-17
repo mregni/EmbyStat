43365c5b1a154a7a961067e41c1daffccb8951dc
Mikhaël Regni <mikhael@uping.be>
2019-07-17 15:08:37 +0200

    Wrong variable structure

--- Scripts/appsettings.ps1
+++ Scripts/appsettings.ps1
@@ -1,6 +1,6 @@
 $appSettings = "EmbyStat.Web\appsettings.json"
 $version = "$($env:GitVersion_SemVer)"
-$rollbarKey = "$($env:webapp.rollbar.key)"
+$rollbarKey = "$($env:webapp_rollbar_key)"
 
 (GC $appSettings).Replace("0.0.0.0", "$version") | Set-Content $appSettings
 (GC $appSettings).Replace("RollbarENV", "dev") | Set-Content $appSettings
