480d3e537a688b13bcd6d13ba42fbd6e305cd98f
Mikhaël Regni <mikhael@uping.be>
2019-07-18 07:29:36 +0200

    Bash script not working yet

--- Scripts/appsettings.sh
+++ Scripts/appsettings.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-sed -i 's/win10-x64-v{version}.zip/$1-v{version}.tar.gz/g' EmbyStat.Web/appsettings.json
+echo "Updating version to $(GitVersion.SemVer)"
 sed -i 's/0.0.0.0/$(GitVersion.SemVer)/g' EmbyStat.Web/appsettings.json
+
+echo "Updating RollbarENV to dev"
 sed -i 's/RollbarENV/dev/g' EmbyStat.Web/appsettings.json
-sed -i 's/XXXXXXXXXX/$2/g' EmbyStat.Web/appsettings.json
+
+echo "Updating Rollbar key to" $2
+sed -i s/XXXXXXXXXX/$2/g EmbyStat.Web/appsettings.json
+
+echo "Updating update package name to" $1
+sed -i s/win10-x64-v{version}.zip/$1-v{version}.tar.gz/g EmbyStat.Web/appsettings.json
