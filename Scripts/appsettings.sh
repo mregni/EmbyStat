847ee0f6533e293a441d74674f682af69447e56b
Mikhaël Regni <mikhael@uping.be>
2019-07-18 09:00:10 +0200

    last fix, I promise

--- Scripts/appsettings.sh
+++ Scripts/appsettings.sh
@@ -4,14 +4,14 @@ package = $1
 rollbarKey = $2
 version = $3
 
-echo "Updating version to " $3
-sed -i 's/0.0.0.0/$version/g' EmbyStat.Web/appsettings.json
+echo "Updating version to " $version
+sed -i "s/0.0.0.0/$version/g" EmbyStat.Web/appsettings.json
 
 echo "Updating RollbarENV to dev"
-sed -i 's/RollbarENV/dev/g' EmbyStat.Web/appsettings.json
+sed -i "s/RollbarENV/dev/g" EmbyStat.Web/appsettings.json
 
-echo "Updating Rollbar key to " $2
-sed -i 's/XXXXXXXXXX/$rollbarKey/g' EmbyStat.Web/appsettings.json
+echo "Updating Rollbar key to " $rollbarKey
+sed -i "s/XXXXXXXXXX/$rollbarKey/g" EmbyStat.Web/appsettings.json
 
-echo "Updating update package name to " $1
-sed -i 's/win10-x64-v{version}.zip/$package-v{version}.tar.gz/g' EmbyStat.Web/appsettings.json
+echo "Updating update package name to " $package
+sed -i "s/win10-x64-v{version}.zip/$package-v{version}.tar.gz/g" EmbyStat.Web/appsettings.json
