be7eeea72ec6b00019b616f263cd16360d09888c
Mikhaël Regni <mikhael@uping.be>
2019-07-18 08:03:21 +0200

    Fixing powershell concat issue

--- Scripts/appsettings.ps1
+++ Scripts/appsettings.ps1
@@ -16,4 +16,4 @@ Write-Host "Updating Rollbar key to $rollbarKey";
 (GC $appSettings).Replace("XXXXXXXXXX", "$rollbarKey") | Set-Content $appSettings
 
 Write-Host "Updating update package name to $updatePackage"
-(GC $appSettings).Replace("win10-x64-v{version}.zip", $updatePackage'-v{version}.zip') | Set-Content $appSettings
+(GC $appSettings).Replace("win10-x64-v{version}.zip", $updatePackage + '-v{version}.zip') | Set-Content $appSettings
