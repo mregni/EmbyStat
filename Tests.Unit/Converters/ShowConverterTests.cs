f9e87b82205212664c0b232a3e1ef691444246a4
Mikhaël Regni <mikhael@uping.be>
2020-01-05 11:25:41 +0100

    fixing unit tests

--- Tests.Unit/Converters/ShowConverterTests.cs
+++ Tests.Unit/Converters/ShowConverterTests.cs
@@ -30,9 +30,57 @@ namespace Tests.Unit.Converters
                 Name = "2 broke girls",
                 ParentId = "123",
                 Path = "c:\\",
-                CommunityRating = 1.2f
+                CommunityRating = 1.2f,
+                DateCreated = new DateTimeOffset(2000, 1, 1, 1, 1, 1, TimeSpan.Zero),
+                ProviderIds = new Dictionary<string, string>
+                {
+                    { "Imdb", "1234" },
+                    { "Tmdb", "1234" },
+                    { "Tvdb", "1234" }
+                },
+                OfficialRating = "16TV",
+                PremiereDate = new DateTimeOffset(2000, 1, 1, 1, 1, 1, TimeSpan.Zero),
+                ProductionYear = 2001,
+                RunTimeTicks = 1000,
+                SortName = "2 broke girls",
+                Status = "Ended",
+                Genres = new []{ "Action" },
+                People = new []
+                {
+                    new BaseItemPerson
+                    {
+                        Id = "123",
+                        Name = "Gimli",
+                        Type = PersonType.Actor
+                    } 
+                }
             };
+
             var show = data.ConvertToShow("123");
+            show.Id.Should().Be(data.Id);
+            show.CollectionId.Should().Be("123");
+            show.Primary.Should().Be(data.ImageTags.FirstOrDefault(y => y.Key == ImageType.Primary).Value);
+            show.Thumb.Should().Be(data.ImageTags.FirstOrDefault(y => y.Key == ImageType.Thumb).Value);
+            show.Logo.Should().Be(data.ImageTags.FirstOrDefault(y => y.Key == ImageType.Logo).Value);
+            show.Banner.Should().Be(data.ImageTags.FirstOrDefault(y => y.Key == ImageType.Banner).Value);
+            show.Name.Should().Be(data.Name);
+            show.ParentId.Should().Be(data.ParentId);
+            show.Path.Should().Be(data.Path);
+            show.CommunityRating.Should().Be(data.CommunityRating);
+            show.DateCreated.Should().Be(data.DateCreated);
+            show.OfficialRating.Should().Be(data.OfficialRating);
+            show.IMDB.Should().Be(data.ProviderIds.FirstOrDefault(y => y.Key == "Imdb").Value);
+            show.TMDB.Should().Be(data.ProviderIds.FirstOrDefault(y => y.Key == "Tmdb").Value);
+            show.TVDB.Should().Be(data.ProviderIds.FirstOrDefault(y => y.Key == "Tvdb").Value);
+            show.PremiereDate.Should().Be(data.PremiereDate);
+            show.ProductionYear.Should().Be(data.ProductionYear);
+            show.RunTimeTicks.Should().Be(data.RunTimeTicks);
+            show.SortName.Should().Be(data.SortName);
+            show.Status.Should().Be(data.Status);
+            show.Genres.Length.Should().Be(1);
+            show.People.Length.Should().Be(1);
+            show.Seasons.Count.Should().Be(0);
+            show.Episodes.Count.Should().Be(0);
         }
 
         [Fact]
@@ -45,7 +93,6 @@ namespace Tests.Unit.Converters
             var virtualEpisode = new VirtualEpisode(showEpisode, showSeason);
             var episode = virtualEpisode.ConvertToEpisode(show, showSeason);
 
-            episode.Id.Should().Be(showEpisode.Id);
             episode.ShowId.Should().Be(show.Id);
             episode.ShowName.Should().Be(show.Name);
             episode.Name.Should().Be(showEpisode.Name);
@@ -53,7 +100,7 @@ namespace Tests.Unit.Converters
             episode.IndexNumber.Should().Be(showEpisode.IndexNumber);
             episode.ParentId.Should().Be(showSeason.Id);
             // ReSharper disable once PossibleInvalidOperationException
-            episode.PremiereDate.Should().Be(showEpisode.PremiereDate.Value.Date);
+            episode.PremiereDate.Should().Be(showEpisode.PremiereDate);
         }
     }
 }
