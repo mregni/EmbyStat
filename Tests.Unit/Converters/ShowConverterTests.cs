e2e2529a2c6e578a583b7f949a9f7158dc5ce498
Mikhaël Regni <mikhael@uping.be>
2020-01-06 07:59:59 +0100

    Updating unit tests

--- Tests.Unit/Converters/ShowConverterTests.cs
+++ Tests.Unit/Converters/ShowConverterTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using EmbyStat.Common.Converters;
+using EmbyStat.Common.Models.Entities;
 using EmbyStat.Common.Models.Show;
 using FluentAssertions;
 using MediaBrowser.Model.Dto;
@@ -81,6 +82,83 @@ namespace Tests.Unit.Converters
             show.People.Length.Should().Be(1);
             show.Seasons.Count.Should().Be(0);
             show.Episodes.Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void ConvertToSeason_Should_Return_Season()
+        {
+            var data = new BaseItemDto
+            {
+                Id = "123",
+                Name = "Season 01",
+                ParentId = "12",
+                Path = "C:\\",
+                DateCreated = new DateTimeOffset(2000, 1, 1, 1, 1, 1, TimeSpan.Zero),
+                IndexNumber = 1,
+                IndexNumberEnd = null,
+                PremiereDate = new DateTimeOffset(2000, 1, 1, 1, 1, 1, TimeSpan.Zero),
+                ProductionYear = 2001,
+                SortName = "season 01",
+                ImageTags = new Dictionary<ImageType, string>
+                {
+                    {ImageType.Primary, "primary"}, {ImageType.Thumb, "thumb"}, {ImageType.Logo, "logo"},
+                    {ImageType.Banner, "banner"}
+                }
+            };
+
+            var season = data.ConvertToSeason();
+            season.Id.Should().Be(data.Id);
+            season.ParentId.Should().Be(data.ParentId);
+            season.Name.Should().Be(data.Name);
+            season.Path.Should().Be(data.Path);
+            season.DateCreated.Should().Be(data.DateCreated);
+            season.IndexNumber.Should().Be(data.IndexNumber);
+            season.IndexNumberEnd.Should().Be(data.IndexNumberEnd);
+            season.PremiereDate.Should().Be(data.PremiereDate);
+            season.ProductionYear.Should().Be(data.ProductionYear);
+            season.SortName.Should().Be(data.SortName);
+            season.Primary.Should().Be(data.ImageTags.FirstOrDefault(y => y.Key == ImageType.Primary).Value);
+            season.Thumb.Should().Be(data.ImageTags.FirstOrDefault(y => y.Key == ImageType.Thumb).Value);
+            season.Logo.Should().Be(data.ImageTags.FirstOrDefault(y => y.Key == ImageType.Logo).Value);
+            season.Banner.Should().Be(data.ImageTags.FirstOrDefault(y => y.Key == ImageType.Banner).Value);
+            season.LocationType.Should().Be(LocationType.Disk);
+        }
+
+        [Fact]
+        public void ConvertToSeason_Should_Return_Season_From_Int()
+        {
+            var seasonIndex = 1;
+            var show = new Show
+            {
+                Id = "1",
+            };
+
+            var season = seasonIndex.ConvertToSeason(show);
+            season.Id.Should().NotBeNullOrWhiteSpace();
+            season.Name.Should().Be($"Season {seasonIndex}");
+            season.ParentId.Should().Be(show.Id);
+            season.Path.Should().Be(string.Empty);
+            season.DateCreated.Should().BeNull();
+            season.IndexNumber.Should().Be(seasonIndex);
+            season.IndexNumberEnd.Should().Be(seasonIndex);
+            season.PremiereDate.Should().BeNull();
+            season.ProductionYear.Should().BeNull();
+            season.SortName.Should().Be("0001");
+            season.LocationType.Should().Be(LocationType.Virtual);
+        }
+
+        [Fact]
+        public void ConvertToSeason_Should_Return_Special_Season_From_Int()
+        {
+            var seasonIndex = 0;
+            var show = new Show
+            {
+                Id = "1",
+            };
+
+            var season = seasonIndex.ConvertToSeason(show);
+            season.Id.Should().NotBeNullOrWhiteSpace();
+            season.Name.Should().Be($"Special");
         }
 
         [Fact]
