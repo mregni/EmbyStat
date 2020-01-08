8a8888a34de60a35d380dde6561ffddf1b92dcb7
Mikhaël Regni <mikhael@uping.be>
2020-01-08 10:12:40 +0100

    Added proper user settings migration, #1039

---
+++ EmbyStat.Migrator/Migrations/0006_EditUserSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EmbyStat.Migrator.Models;
+
+namespace EmbyStat.Migrator.Migrations
+{
+    [Migration(6)]
+    public class EditUserSettings : Migration
+    {
+        public override void Up()
+        {
+            if (UserSettings.Emby != null)
+            {
+                UserSettings.MediaServer = UserSettings.Emby;
+                UserSettings.Emby = null;
+            }
+        }
+    }
+}
