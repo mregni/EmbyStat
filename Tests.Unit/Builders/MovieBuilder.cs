d629b031f93112e33aa9b21814cba5aa8b546b8a
Mikhaël Regni <mikhael@uping.be>
2020-03-25 11:41:08 +0100

    Fixing unit tests

--- Tests.Unit/Builders/MovieBuilder.cs
+++ Tests.Unit/Builders/MovieBuilder.cs
@@ -25,7 +25,7 @@ namespace Tests.Unit.Builders
                 RunTimeTicks = 120000000000,
                 Primary = "primaryImage",
                 IMDB = "0001",
-                Video3DFormat = null,
+                Video3DFormat = Video3DFormat.None,
                 Genres = new[] { "id1" },
                 People = new[] { new ExtraPerson { Id = Guid.NewGuid().ToString(), Name = "Gimli", Type = PersonType.Actor } },
                 MediaSources = new List<MediaSource> { new MediaSource { SizeInMb = 1001 } },
