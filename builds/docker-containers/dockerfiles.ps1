886064d7f43b05cc3cf2a9e5e87a5613aeb1fb3a
Mikhaël Regni <mikhael@uping.be>
2020-08-10 11:48:08 +0200

    Fixing copy powershell script

--- builds/docker-containers/dockerfiles.ps1
+++ builds/docker-containers/dockerfiles.ps1
@@ -1,7 +1,7 @@
 New-Item -Path "$($env:System_DefaultWorkingDirectory)" -Name "publish" -ItemType "directory"
 
-Copy-Item "builds\docker-containers\files\win.dockerfile" -Destination "$($env:System_DefaultWorkingDirectory)\publish\docker\win.dockerfile"
-Copy-Item "builds\docker-containers\files\linux.dockerfile" -Destination "$($env:System_DefaultWorkingDirectory)\publish\docker\linux.dockerfile"
-Copy-Item "builds\docker-containers\files\manifest-tool-windows-amd64.exe" -Destination "$($env:System_DefaultWorkingDirectory)\publish\docker\manifest-tool-windows-amd64.exe"
-Copy-Item "builds\docker-containers\files\manifest-nightly.yaml" -Destination "$($env:System_DefaultWorkingDirectory)\publish\docker\manifest-nightly.yaml"
-Copy-Item "builds\docker-containers\files\manifest-beta.yaml" -Destination "$($env:System_DefaultWorkingDirectory)\publish\docker\manifest-beta.yaml"
+Copy-Item ".\builds\docker-containers\files\win.dockerfile" -Destination "$($env:System_DefaultWorkingDirectory)\publish\win.dockerfile"
+Copy-Item ".\builds\docker-containers\files\linux.dockerfile" -Destination "$($env:System_DefaultWorkingDirectory)\publish\linux.dockerfile"
+Copy-Item ".\builds\docker-containers\files\manifest-tool-windows-amd64.exe" -Destination "$($env:System_DefaultWorkingDirectory)\publish\manifest-tool-windows-amd64.exe"
+Copy-Item ".\builds\docker-containers\files\manifest-nightly.yaml" -Destination "$($env:System_DefaultWorkingDirectory)\publish\manifest-nightly.yaml"
+Copy-Item ".\builds\docker-containers\files\manifest-beta.yaml" -Destination "$($env:System_DefaultWorkingDirectory)\publish\manifest-beta.yaml"
