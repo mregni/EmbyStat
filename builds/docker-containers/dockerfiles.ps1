f714305ca908a744267307a5951c44d1f1c95848
Mikhaël Regni <mikhael@uping.be>
2022-04-28 16:45:46 +0200

    Trying new release config

--- builds/docker-containers/dockerfiles.ps1
+++
@@ -1,8 +0,0 @@
-New-Item -Path "$($env:System_DefaultWorkingDirectory)" -Name "publish" -ItemType "directory"
-
-Copy-Item ".\builds\docker-containers\files\win.dockerfile" -Destination "$($env:System_DefaultWorkingDirectory)\publish\win.dockerfile"
-Copy-Item ".\builds\docker-containers\files\arm64.dockerfile" -Destination "$($env:System_DefaultWorkingDirectory)\publish\arm64.dockerfile"
-Copy-Item ".\builds\docker-containers\files\linux.dockerfile" -Destination "$($env:System_DefaultWorkingDirectory)\publish\linux.dockerfile"
-Copy-Item ".\builds\docker-containers\files\manifest-tool-windows-amd64.exe" -Destination "$($env:System_DefaultWorkingDirectory)\publish\manifest-tool-windows-amd64.exe"
-Copy-Item ".\builds\docker-containers\files\manifest-nightly.yaml" -Destination "$($env:System_DefaultWorkingDirectory)\publish\manifest-nightly.yaml"
-Copy-Item ".\builds\docker-containers\files\manifest-beta.yaml" -Destination "$($env:System_DefaultWorkingDirectory)\publish\manifest-beta.yaml"
