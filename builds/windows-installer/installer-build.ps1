1cb42ef1c693ed2f042d89c2b48195e28534c6f4
Mikhaël Regni <mikhael@uping.be>
2022-05-02 13:40:03 +0200

    Going back to 1.2

--- builds/windows-installer/installer-build.ps1
+++ builds/windows-installer/installer-build.ps1
@@ -73,7 +73,7 @@ function Make-NSIS {
 
 function Install-NSIS {
     Write-Host "Downloading NSIS"
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls13
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     Invoke-WebRequest -Uri https://nchc.dl.sourceforge.net/project/nsis/NSIS%203/3.04/nsis-3.04.zip -UseBasicParsing -SkipCertificateCheck -OutFile "$tempdir/nsis.zip" | Write-Host
 
     Expand-Archive "$tempdir/nsis.zip" -DestinationPath "$tempdir/nsis/" -Force | Write-Host
